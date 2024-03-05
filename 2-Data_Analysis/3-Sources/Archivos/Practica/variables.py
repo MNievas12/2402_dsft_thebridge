241f95c4e4badc08cead3e5f275d88ed25904381
Miguel Nievas <miguel.nievas.fuertes@gmail.com>
2024-03-05 11:14:44 +0100

    W5 D2 sol

---
+++ 2-Data_Analysis/3-Sources/Archivos/Practica/variables.py
@@ -0,0 +1,10 @@
+
+carpeta = 'D:\\Bootcamp\\Carpeta_profesor\\2402_dsft_thebridge\\2-Data_Analysis\\3-Sources\\Archivos\\Practica\\otra_carpeta'
+
+
+categorias = {
+            "Imagenes": ('.jpg', '.jpeg', '.png', '.svg', '.gif'),
+              "Documentos": ('.doc', '.docx', '.txt', '.pdf', '.xls', '.ppt', '.xlsx', '.pptx'),
+              "Software": ('.exe', '.py','.ipynb'),
+              "Datasets": (".csv"),
+              "Otros": ()}
